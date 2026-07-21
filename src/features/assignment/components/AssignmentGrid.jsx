e74b9a31fad9d256ebcc734a351aa9fe84075957
sekhubede <msekhubede@gmail.com>
2026-07-21 14:31:23 +0200

    feat: assignment sliding detail panel
    - Added DetailPanel with more details about the assignment
    - DetailPanel shows subtasks, comments and has assignment action buttons
    - Added DetailPanel.css to style the DetailPanel, status and priority into a grid style for better visuals

--- src/features/assignment/components/AssignmentGrid.jsx
+++ src/features/assignment/components/AssignmentGrid.jsx
@@ -1,14 +1,39 @@
 import AssignmentCard from "./AssignmentCard";
 import { ASSIGNMENTS } from "../data/data";
+import { useState } from 'react';
+import DetailPanel from './DetailPanel';
 
 function AssignmentGrid() {
+  const [selectedAssignment, setSelectedAssignment] = useState(null);
+  const [isPanelOpen, setIsPanelOpen] = useState(false);
+
+  const handleCardClick = (assignment) => {
+    setSelectedAssignment(assignment);
+    setIsPanelOpen(true);
+  }
+
+  const handleClosePanel = () => {
+    setIsPanelOpen(false);
+    setTimeout(() => setSelectedAssignment(null), 300);
+  }
+
   return (
     <div>
       <ul className="assignment-grid">
         {ASSIGNMENTS.map((assignment) => (
-          <AssignmentCard key={assignment.id} assignment={assignment} />
+          <AssignmentCard 
+            key={assignment.id} 
+            assignment={assignment} 
+            onCardClick={handleCardClick}
+          />
         ))}
       </ul>
+
+      <DetailPanel 
+        assignment={selectedAssignment}
+        isOpen={isPanelOpen}
+        onClose={handleClosePanel}
+      />
     </div>
   );
 }
